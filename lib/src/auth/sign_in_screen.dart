0b1cb747ddbf69b496efb82f304481298b973aac
Felipe Monteiro <felipe.monteiro@finansystech.com>
2023-01-21 17:13:07 -0300

    Sign Up Screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -150,7 +151,13 @@ class SignInScreen extends StatelessWidget {
                                 borderRadius: BorderRadius.circular(18)),
                             side: const BorderSide(
                                 width: 2, color: Colors.green)),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (c) {
+                              return SignUpScreen();
+                            }),
+                          );
+                        },
                         child: const Text(
                           'Criar Conta',
                           style: TextStyle(fontSize: 18),
