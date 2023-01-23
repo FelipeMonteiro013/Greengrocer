857ed8e7f0a09261577ea499c69574be4373e85b
Felipe Monteiro <felipe.monteiro@finansystech.com>
2023-01-23 00:03:19 -0300

    Product Screen

---
+++ lib/src/pages/widgets/quantity_widget.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/models/item_model.dart';
+
+class QuantityWidget extends StatelessWidget {
+  final int value;
+  final String suffixText;
+  final Function(int quantity) result;
+
+  const QuantityWidget({
+    Key? key,
+    required this.value,
+    required this.suffixText,
+    required this.result,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              spreadRadius: 1,
+              blurRadius: 2,
+            ),
+          ]),
+      child: Row(
+        children: [
+          _QuantityButtom(
+            color: Colors.grey,
+            icon: Icons.remove,
+            onPressed: () {
+              if (value == 1) return;
+
+              int resultCount = value - 1;
+              result(resultCount);
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 6),
+            child: Text(
+              '$value$suffixText',
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          _QuantityButtom(
+            color: CustomColors.customSwatchColor,
+            icon: Icons.add,
+            onPressed: () {
+              int resultCount = value + 1;
+              result(resultCount);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _QuantityButtom extends StatelessWidget {
+  const _QuantityButtom({
+    Key? key,
+    required this.color,
+    required this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final Color color;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        borderRadius: BorderRadius.circular(50),
+        onTap: onPressed,
+        child: Ink(
+          width: 25,
+          height: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+          child: Icon(
+            icon,
+            color: Colors.white,
+            size: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
